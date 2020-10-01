f2a19dcaf217fcb1f5d5bd91a2bf87a338bcf328
Don <64542224+penguin1923@users.noreply.github.com>
2020-10-01 11:00:10 -0400

    JS doodler and platforms made

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGameOver = false
+    let platformCount = 5
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+    class Platform {
+        constructor(newPlatformBottom) {
+            this.bottom = newPlatformBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++) {
+            let platformGap = 600 / platformCount
+            let newPlatformBottom = 100 + i * platformGap
+            let newPlatform = new Platform(newPlatformBottom)
+        }
+    }
+
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+            createPlatforms()
+        }
+    }
+    //attach a button
+    start()
+})
